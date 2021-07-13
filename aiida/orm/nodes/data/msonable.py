5b0a04063e849bb8a299577c07a330dd7843f95a
Bonan Zhu <zhubonan@outlook.com>
2021-07-13 21:58:37 +0100

    Added support for NaN and inf and numpy arrays

--- aiida/orm/nodes/data/msonable.py
+++ aiida/orm/nodes/data/msonable.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """Data plugin for classes that implement the ``MSONable`` class of the ``monty`` library."""
+#pylint= disable=unused-import, arguments-differ
 import importlib
 
-from monty.json import MSONable
+import numpy as np
+
+from monty.json import MSONable, MontyDecoder, _serialize_callable
+
+try:
+    import bson
+except ImportError:
+    bson = None
 
 from aiida.orm import Data
 
@@ -44,7 +52,8 @@ class MsonableData(Data):
         super().__init__(*args, **kwargs)
 
         self._obj = obj
-        self.set_attribute_many(obj.as_dict())
+        preprocess = JSONPreprocessor()  # Adds additional support for NaN, -inf, simple numpy arrays and more
+        self.set_attribute_many(preprocess.process(obj.as_dict()))
 
     def _get_object(self):
         """Return the cached wrapped MSONable object.
@@ -56,7 +65,8 @@ class MsonableData(Data):
         try:
             return self._obj
         except AttributeError:
-            attributes = self.attributes
+            postprocess = JSONPostprocessor()
+            attributes = postprocess.process(self.attributes)
             class_name = attributes['@class']
             module_name = attributes['@module']
 
@@ -79,3 +89,115 @@ class MsonableData(Data):
     def obj(self):
         """Return the wrapped MSONable object."""
         return self._get_object()
+
+
+class JSONPreprocessor:
+    """Preprocessor for making the representation JSON compatible"""
+
+    def __init__(self):
+        """Instantiate an Preprocessor object"""
+        super()
+        self.process_funcs = [self.process_additional, self.process_nan]
+
+    @staticmethod
+    def process_nan(value):
+        """Recursively encode nan values"""
+        inf = float('inf')
+        if value == inf:
+            return 'Infinity'
+        if value == -inf:
+            return '-Infinity'
+        if value != value:  # pylint: disable=comparison-with-itself
+            return 'NaN'
+        return value
+
+    @staticmethod
+    def process_additional(obj):  # Too
+        """Preprocessing the dict representation before serialisation"""
+        import datetime
+        from uuid import UUID
+
+        if isinstance(obj, datetime.datetime):
+            obj = {'@module': 'datetime', '@class': 'datetime', 'string': obj.__str__()}
+        elif isinstance(obj, UUID):
+            obj = {'@module': 'uuid', '@class': 'UUID', 'string': obj.__str__()}
+
+        if np is not None:
+            if isinstance(obj, np.ndarray):
+                if str(obj.dtype).startswith('complex'):
+                    obj = {
+                        '@module': 'numpy',
+                        '@class': 'array',
+                        'dtype': obj.dtype.__str__(),
+                        'data': [obj.real.tolist(), obj.imag.tolist()],
+                    }
+                else:
+                    obj = {
+                        '@module': 'numpy',
+                        '@class': 'array',
+                        'dtype': obj.dtype.__str__(),
+                        'data': obj.tolist(),
+                    }
+            elif isinstance(obj, np.generic):
+                return obj.item()
+
+        if bson is not None:
+            if isinstance(obj, bson.objectid.ObjectId):
+                obj = {'@module': 'bson.objectid', '@class': 'ObjectId', 'oid': str(obj)}
+
+        if callable(obj) and not isinstance(obj, MSONable):
+            return _serialize_callable(obj)
+        return obj
+
+    def process(self, obj):
+        """Preprocessing before saving the object as a JSON in PostgreSQL"""
+        # Process nested list and dictionary
+        if isinstance(obj, list):
+            return [self.process(item) for item in obj]
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                obj[key] = self.process(value)
+            return obj
+        # Apply processor methods
+        for processor in self.process_funcs:
+            obj = processor(obj)
+        return obj
+
+
+class JSONPostprocessor:
+    """Preprocessor for the JSON object loaded from PostgreSQL for supporting additional data types"""
+
+    def __init__(self):
+        """Instantiate an Postprocessor object"""
+        self.process_funcs = [self.process_nan, self.process_additional]
+
+    @staticmethod
+    def process_nan(obj):
+        """Recursively decode  dict loaded from JSON with nan values"""
+        inf = float('inf')
+        if obj == 'Infinity':
+            return inf
+        if obj == '-Infinity':
+            return -inf
+        if obj == 'NaN':
+            return float('nan')
+        return obj
+
+    @staticmethod
+    def process_additional(obj):
+        """Post-processing for supporting additional data types after deserialization"""
+        return MontyDecoder().process_decoded(obj)
+
+    def process(self, obj):
+        """Preprocessing before saving the object as a JSON in PostgreSQL"""
+        # Process nested list and dictionary
+        if isinstance(obj, list):
+            return [self.process(item) for item in obj]
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                obj[key] = self.process(value)
+            return obj
+        # Apply processor methods
+        for processor in self.process_funcs:
+            obj = processor(obj)
+        return obj
